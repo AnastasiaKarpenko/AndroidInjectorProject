f7c06b6f5f4099626725837ab65a3a44b2711fab
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-13 17:19:58 +0200

    Injected MainActivity

--- app/src/main/java/ws/tilda/anastasia/androidinjectorproject/activity/MainActivity.java
+++ app/src/main/java/ws/tilda/anastasia/androidinjectorproject/activity/MainActivity.java
@@ -1,14 +1,19 @@
 package ws.tilda.anastasia.androidinjectorproject.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import dagger.android.AndroidInjection;
 import ws.tilda.anastasia.androidinjectorproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    //@Inject member1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AndroidInjection.inject(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
