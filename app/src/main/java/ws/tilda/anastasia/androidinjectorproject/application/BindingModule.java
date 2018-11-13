15adf651ae01f6d322f277125b03109df7b26970
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-13 16:33:08 +0200

    Created an abstract method to bind MainActivity subcomponent into the application component

--- app/src/main/java/ws/tilda/anastasia/androidinjectorproject/application/BindingModule.java
+++ app/src/main/java/ws/tilda/anastasia/androidinjectorproject/application/BindingModule.java
@@ -1,7 +1,20 @@
 package ws.tilda.anastasia.androidinjectorproject.application;
 
+import android.app.Activity;
+
+import dagger.Binds;
 import dagger.Module;
+import dagger.android.ActivityKey;
+import dagger.android.AndroidInjector;
+import dagger.multibindings.IntoMap;
+import ws.tilda.anastasia.androidinjectorproject.activity.ActivitySubComponent;
+import ws.tilda.anastasia.androidinjectorproject.activity.MainActivity;
 
 @Module
-public class BindingModule {
+public abstract class BindingModule {
+
+    @Binds
+    @IntoMap
+    @ActivityKey(MainActivity.class)
+    abstract AndroidInjector.Factory<? extends Activity> bindMainActivity(ActivitySubComponent.Builder builder);
 }
